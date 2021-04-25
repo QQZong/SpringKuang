5e7d6e4f5733e3335041bfadead83ed79173ce2d
qinzong <1003326090@qq.com>
2021-04-25 20:22:37 +0800

    使用注解管理了实现类的注册

--- spring-01-ioc/src/test/java/MyTest.java
+++ spring-01-ioc/src/test/java/MyTest.java
@@ -1,13 +1,17 @@
 
-import com.qin.dao.daoImpl.UserDaoMysql;
-import com.qin.dao.daoImpl.UserDaoOracle;
-import com.qin.server.UserServer;
 import com.qin.server.serverImpl.UserServerImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
     public static void main(String[] args) {
-        UserServer userServer =  new UserServerImpl();
+        /*IOC方式*/
+        /*UserServer userServer =  new UserServerImpl();
         ((UserServerImpl) userServer).setUserDao(new UserDaoOracle());
-        userServer.getData();
+        userServer.getData();*/
+        /*Spring方式*/
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserServerImpl server = (UserServerImpl) context.getBean("server");
+        server.getData();
     }
 }
